2ad1fdd2591aa720c3be7f495e3b71c973b4d5fe
Mazvydas Tadaravicius <mazvydas.tadaravicius@gmail.com>
2019-11-27 23:47:49 +0000

    REST UI component JSON data field provides data to either POST body ar GET params

--- src/app/rest-ui/rest-ui.component.ts
+++ src/app/rest-ui/rest-ui.component.ts
@@ -1,10 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { HttpRequest, HttpParams } from '@angular/common/http';
 import { FormControl } from '@angular/forms';
 
-import { flatMap, startWith, shareReplay, withLatestFrom, scan, take } from 'rxjs/operators';
-import { combineLatest, of, Subject, Observable } from 'rxjs';
+import { startWith, shareReplay, withLatestFrom, scan, first, flatMap, tap, skip } from 'rxjs/operators';
+import { combineLatest, Subject, Observable, of } from 'rxjs';
 
 import { SharepointRestService, IRESTRequest } from '../sharepoint-rest.service';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 interface Query {
   request: string,
@@ -27,23 +29,28 @@ export class RestUiComponent {
     { value: `application/atom+xml`, text: `XML` }*/
   ];
 
-  baseURLControl = new FormControl(`../../`);
-  restQueryControl = new FormControl(`_api/web/lists/getbytitle('Programs')/items?$select=Title,Platform/Title&$expand=Platform/Id&$filter=(substringof('mid',Platform/Title) and substringof('ml',Platform/Title))`);
-  sendMethodControl = new FormControl(this.sendMethods[0]);
-  acceptHeaderControl = new FormControl(this.headerValues[0].value);
-  contentTypeHeaderControl = new FormControl(this.headerValues[0].value);
-  postDataControl = new FormControl(``);
+  readonly baseURLControl = new FormControl(`../../`);
+  readonly restQueryControl = new FormControl(`_api/web/lists/getbytitle('Programs')/items`);
+  readonly sendMethodControl = new FormControl(this.sendMethods[0]);
+  readonly acceptHeaderControl = new FormControl(this.headerValues[0].value);
+  readonly contentTypeHeaderControl = new FormControl(this.headerValues[0].value);
+  readonly postDataControl = new FormControl(`{
+    $select: "Title,Platform/Title",
+    $expand: "Platform/Id",
+    $filter: "(substringof('mid',Platform/Title) and substringof('ml',Platform/Title))"
+  }`);
 
-  baseURLObservable: Observable<string> = this.baseURLControl.valueChanges.pipe(startWith(this.baseURLControl.value), shareReplay(1));
-  restQueryObservable: Observable<string> = this.restQueryControl.valueChanges.pipe(startWith(this.restQueryControl.value), shareReplay(1));
-  sendMethodObservable: Observable<'DELETE' | 'GET' | 'PATCH' | 'POST'> = this.sendMethodControl.valueChanges.pipe(startWith(this.sendMethodControl.value), shareReplay(1));
-  acceptHeaderObservable: Observable<string> = this.acceptHeaderControl.valueChanges.pipe(startWith(this.acceptHeaderControl.value), shareReplay(1));
-  contentTypeObservable: Observable<string> = this.contentTypeHeaderControl.valueChanges.pipe(startWith(this.contentTypeHeaderControl.value), shareReplay(1));
-  postDataObservable: Observable<string> = this.postDataControl.valueChanges.pipe(startWith(this.postDataControl.value), shareReplay(1));
-  sendObservable = new Subject<void>();
+  readonly baseURLObservable: Observable<string> = this.baseURLControl.valueChanges.pipe(startWith(this.baseURLControl.value), shareReplay(1));
+  readonly restQueryObservable: Observable<string> = this.restQueryControl.valueChanges.pipe(startWith(this.restQueryControl.value), shareReplay(1));
+  readonly sendMethodObservable: Observable<'DELETE' | 'GET' | 'PATCH' | 'POST'> = this.sendMethodControl.valueChanges.pipe(startWith(this.sendMethodControl.value), shareReplay(1));
+  readonly acceptHeaderObservable: Observable<string> = this.acceptHeaderControl.valueChanges.pipe(startWith(this.acceptHeaderControl.value), shareReplay(1));
+  readonly contentTypeObservable: Observable<string> = this.contentTypeHeaderControl.valueChanges.pipe(startWith(this.contentTypeHeaderControl.value), shareReplay(1));
+  readonly postDataObservable: Observable<string> = this.postDataControl.valueChanges.pipe(startWith(this.postDataControl.value), shareReplay(1));
+  readonly sendObservable = new Subject<void>();
 
-  postDataObjectObservable = this.postDataObservable.pipe(
+  readonly postDataObjectObservable = this.postDataObservable.pipe(
     flatMap(text => {
+      text = text.trim();
       try {
         let postDataObject: {} = eval(`() => {return ${text}}`)();
 
@@ -57,7 +64,7 @@ export class RestUiComponent {
     shareReplay(1)
   );
 
-  requestObjectObservable = combineLatest(
+  readonly requestObjectObservable = combineLatest(
     combineLatest(this.baseURLObservable, this.restQueryObservable)
       .pipe(flatMap(values => of(values[0] + values[1]))),
     this.sendMethodObservable,
@@ -80,9 +87,30 @@ export class RestUiComponent {
         method: sendMethod
       };
 
-      if ((sendMethod === `POST` || sendMethod === `PATCH`) && postData) {
-        requestObject.headers[`Content-Type`] = values[3];
-        requestObject.bodyOrParams = postData;
+      if (postData) {
+        if ((sendMethod === `POST` || sendMethod === `PATCH`)) {
+          requestObject.headers[`Content-Type`] = values[3];
+          requestObject.bodyOrParams = postData;
+        }
+
+        else if (sendMethod === `GET`) {
+          let indexOfQuestionMark = requestObject.url.indexOf(`?`);
+
+          let params = (indexOfQuestionMark > -1 ? requestObject.url.substring(indexOfQuestionMark + 1) : ``)
+            .split(`#`)[0]
+            .split(`&`)
+            .filter(value => value.length > 0)
+            .map(pair => pair.split(`=`))
+            .reduce((params, param) => {
+              params[param[0]] = param[1];
+              return params;
+            }, {} as any);
+
+          Object.assign(params, postData);
+
+          const httpRequest = new HttpRequest("GET", requestObject.url.split(`?`)[0].split(`#`)[0], { params: new HttpParams({ fromObject: params }) });
+          requestObject.url = httpRequest.urlWithParams;
+        }
       }
 
       return of(requestObject);
@@ -90,7 +118,7 @@ export class RestUiComponent {
     shareReplay(1)
   );
 
-  queriesObservable = this.sendObservable.pipe(
+  readonly queriesObservable = this.sendObservable.pipe(
     withLatestFrom(this.requestObjectObservable),
     flatMap(dataArray => of(dataArray[1])),
     scan<IRESTRequest, Query[]>((queries, request) => {
@@ -105,7 +133,19 @@ export class RestUiComponent {
     }, [])
   );
 
-  constructor(private restService: SharepointRestService) { }
+  readonly postDataControlErrorStateMatcher: ErrorStateMatcher = {
+    isErrorState: control => control.invalid
+  };
+
+  constructor(private restService: SharepointRestService) {
+    this.postDataControl.setAsyncValidators(() => {
+      return this.postDataObjectObservable.pipe(
+        skip(1),
+        flatMap(postDataObject => of(this.isString(postDataObject) ? { dataObjectError: postDataObject } : null)),
+        first()
+      );
+    });
+  }
 
   isString(value: any) {
     return typeof value === `string` || value instanceof String;
